76bef4909b7ea0a8607d993cbda341301fe6af57
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2020-08-10 01:21:33 +0200

    Adding definition of field

--- src/fields/field.hpp
+++ src/fields/field.hpp
@@ -9,10 +9,37 @@
 # include "config.hpp"
 #endif
 
+#include <tensors/tensor.hpp>
+#include <tensors/componentsList.hpp>
+
 namespace ciccios
 {
+  DEFINE_FEATURE(IsField);
   
+  DEFINE_FEATURE_GROUP(FieldFeat);
   
+  /// Field: a tensor with spacetime tipe
+  template <typename SPComp,
+	    typename Comps,
+	    typename Fund=double,
+	    StorLoc SL=DefaultStorage>
+  struct Field;
+  
+  /// Short name for the field
+#  define THIS					\
+  Field<TensComps<TC...>,SPComp,Fund,SL>
+  
+  /// Field
+  template <typename SPComp,
+	    typename...TC,
+	    typename Fund,
+	    StorLoc SL>
+  struct THIS : public
+    FieldFeat<IsField,THIS>
+  {
+  };
+  
+#undef THIS
 }
 
 #endif
