613b887edff7656031ccf54875e207dea2bddbd2
Francesco Sanfilippo <fr.sanfilippo@gmail.com>
2020-08-22 00:55:20 +0200

    Adding product components

--- src/expr/product.hpp
+++ src/expr/product.hpp
@@ -6,6 +6,10 @@
 /// \brief Implements product of expressions
 
 #include <expr/expr.hpp>
+#include <tensors/component.hpp>
+#include <tensors/componentsList.hpp>
+#include <tensors/tensor.hpp>
+#include <utilities/tuple.hpp>
 
 namespace ciccios
 {
@@ -24,17 +28,112 @@ namespace ciccios
       Product<U1,U2>(u1.deFeat(),u2.deFeat());
   }
   
+  namespace impl
+  {
+    /// Computes the resulting components
+    template <typename Comps1,
+	      typename Comps2>
+    struct ProductComps
+    {
+      /// Gets col components from 1
+      using Cln1=
+	TensCompsFilterCln<Comps1>;
+      
+      /// Gets row components from 2
+      using Row2=
+	TensCompsFilterRow<Comps2>;
+      
+      /// List contracted components
+      using ContractedComps=
+	 TupleCommonTypes<TensCompsTransp<Cln1>,Row2>;
+      
+      /// Filter out the contracted components from Comps1
+      using F1FilteredContractedComps=
+      	TupleFilterOut<TensCompsTransp<ContractedComps>,Comps1>;
+      
+      /// Filter out the contracted components from Comps2
+      using F2FilteredContractedComps=
+      	TupleFilterOut<ContractedComps,Comps2>;
+      
+      /// Get unique types of F2Filtered not contained in F1Filtered
+      using F2UniqueFilteredComps=
+      	TupleFilterOut<F1FilteredContractedComps,F2FilteredContractedComps>;
+      
+      /// Merge components catting them
+      using MergedComps=
+      	TupleCat<F1FilteredContractedComps,F2UniqueFilteredComps>;
+      
+      /// Number of merged components
+      static constexpr int nMergedComps=
+	std::tuple_size<MergedComps>::value;
+      
+      /// Check if we can rearrange components as Comps1
+      static constexpr bool CanBeRearrangedAs1=
+	std::tuple_size<TupleCommonTypes<Comps1,MergedComps>>::value==nMergedComps and
+	std::tuple_size<Comps1>::value==nMergedComps;
+      
+      /// Check if we can rearrange components as Comps2
+      static constexpr bool CanBeRearrangedAs2=
+	std::tuple_size<TupleCommonTypes<Comps2,MergedComps>>::value==nMergedComps and
+	std::tuple_size<Comps1>::value==nMergedComps;
+      
+      /// Resulting components is Comps1, Comps2, or MergedComps if
+      /// none of the firsts is a reordered version of the latter
+      ///
+      /// \todo Improve
+      using Comps=
+	std::conditional_t<CanBeRearrangedAs1,Comps1,
+			   std::conditional_t<CanBeRearrangedAs2,Comps2,MergedComps>>;
+    };
+  }
+  
+  /// Product of two expressions
   template <typename F1,
 	    typename F2>
   struct Product : Expr<Product<F1,F2>>
   {
+    /// First expression
     const F1& f1;
+    
+    /// Second expression
     const F2& f2;
     
+    /// Resulting fundamental type
+    using Fund=
+      std::common_type_t<typename F1::Fund,
+			 typename F2::Fund>;
+    
+    /// Resulting components
+    using Comps=
+      typename impl::ProductComps<typename F1::Comps,typename F2::Comps>::Comps;
+    
+    /// Construct taking two expressions
     Product(const F1& f1,
 	    const F2& f2)
       : f1(f1),f2(f2)
     {
+    }
+
+    //va aggiunto l'operatore sottoscrizione, che prende la componente
+    //dal primo operando se e' riga, dal secondo se e' colonna, o da
+    //tutt e due se e' any. Quando non ci sono piu componenti
+    //disponibili, la contrazione degli indici colonna/riga avviene.
+    //Quando si assegna, l'operatore di assegnazione procedera' a
+    //fissare i vari indici dell'output. Poi un giorno si puo'
+    //scrivere anche l'assegnazione di un prodotto, che ciclera'
+    //diversamente
+    
+    /// Occorre far si che il costruttore di copie di un tensore
+    /// effettui effettivamente una copia, e che anche gli stack e
+    /// dynamic storage lo facciano, e facciamo si che importare in un
+    /// kernel sia esplicito
+    
+    auto close()
+      const
+    {
+	Tens<Comps,Fund> a;
+	
+	return a;
     }
   };
 }
